5894fb7d01610a0e31276b6de0ad3ba6dcd95642
GOWTHAM-MG-2006 <mggowtham2006@gmail.com>
2026-08-01 00:38:13 +0530

    mod(bank_console): two execptions added

--- bank_console.py
+++ bank_console.py
@@ -1,9 +1,19 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Account:
     id: int
     customer_name: str
     balance: float
 
-account: dict[int,Account]={}
+
+accounts: dict[int, Account] = {}
+
+
+class AccountNotFoundError(Exception):
+    pass
+
+
+class InsufficientFundsError(Exception):
+    pass
